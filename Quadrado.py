db15a106d47f94670e74a8cef28590b391513cc0
AlissonKawan <rm567598@fiap.com.br>
2025-08-29 16:10:09 -0300

    Update Quadrado.py

--- Quadrado.py
+++ Quadrado.py
@@ -2,4 +2,5 @@ Aresta = int(input('Digite o valor da aresta desse quadrado: '))
 
 Area = (Aresta ** 2)
 
-print(f'Então o valor da area desse quadrado é: {Area}')
+print(f'Então o valor da area desse quadrado é: {Area}m²')
+
